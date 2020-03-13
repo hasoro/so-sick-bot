8114344092f4691408d165a6d9914ee9c4c1f873
hasoro <62137730+hasoro@users.noreply.github.com>
2020-03-13 19:19:35 +0900

    Add files via upload

---
+++ bot maker.py
@@ -0,0 +1,32 @@
+import discord
+import random
+client = discord.Client()
+x=0
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print('ready')
+    game = discord.Game("\"안녕\"이라고 인사하세요!")
+    await client.change_presence(status=discord.Status.online,activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("말걸지마라 애송이")
+    if message.content.startswith("시발"):
+        await message.channel.send("욕하지마라 애송이:middle_finger:")
+    if message.content.startswith("주사위"):
+        x = 0
+        x = random.randint(1, 6)
+        await message.channel.send("%d"%x)
+    if message.content.startswith("동전"):
+        x = 0
+        x = random.randint(1, 2)
+        if x == 1 :
+            await message.channel.send("앞면")
+        else:
+            await message.channel.send("뒷면")
+
+
+
+client.run("Njg3MTU4NzU5MDAwMTEzMTUy.XmhtDA.wB4eKqRVkL3kRfOaiyB9basn4cg")
